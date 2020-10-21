820607ab0676aacc47fb9adcd99802a2aa430b7c
smile021204 <70319701+smile021204@users.noreply.github.com>
2020-10-21 18:11:11 +0900

    Update 2443_별 찍기 - 6.c

--- BOJ/구현/2443_별 찍기 - 6.c
+++ BOJ/구현/2443_별 찍기 - 6.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-int main()
 int main ()
 {
-    int numberOfStars;
-    scanf("%d", &numberOfStars);
-    for (int i=1;i<=numberOfStars;i++) {
-        for(int j=numberOfStars-i;j>0;j--){
     int stars;
     int i, j;
     scanf("%d",&stars);
@@ -16,7 +11,6 @@ int main ()
         for (j = stars - i; j > 0; j--){
             printf(" ");
         }
-        for(int j = 1;j<=2*i-1;j++){
         for (j = 0; j < 2 * i - 1; j++) {
             printf("*");
         }
